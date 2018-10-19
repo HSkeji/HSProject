3d28ac6e76a77f3ca41ba0010f3ca3a3131b7e50
HSkeji <huashanede@163.com>
2018-10-19 19:27:44 +0800

    WX添加了一个Person类

---
+++ HSThirdProject/HSThirdProject/HSPerson.m
@@ -0,0 +1,13 @@
+//
+//  HSPerson.m
+//  HSThirdProject
+//
+//  Created by 王泽 on 2018/10/19.
+//  Copyright © 2018年 Huashankeji. All rights reserved.
+//
+
+#import "HSPerson.h"
+
+@implementation HSPerson
+
+@end
